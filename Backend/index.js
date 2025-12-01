08f5fa2a582815ae1faa99c6c908ddad5c40e33e
321sar <sarvajeetchauhan115@gmail.com>
2025-12-01 20:33:48 +0530

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -15,10 +15,8 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: "https://ink-pixel.onrender.com",
+  origin: ["*"],
   credentials: true,
-   methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type, Authorization",
 }));
 
 
