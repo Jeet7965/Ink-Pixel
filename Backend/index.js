89281b9d9994d19dd13247ad9a849d013c98424f
321sar <sarvajeetchauhan115@gmail.com>
2025-12-02 12:59:42 +0530

    origing setup for cors error

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: ["http://localhost:5173","https://ink-pixel.onrender.com","http://localhost:5174"],
+  origin: ["http://localhost:5173","https://inkpixel.netlify.app","http://localhost:5174"],
   credentials: true,
 }));
 
