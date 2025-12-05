c69093a5a172207afee5be4042e6c7d17ac7b1dc
321sar <sarvajeetchauhan115@gmail.com>
2025-12-05 13:15:29 +0530

    changes in pagination api.

--- frontend/src/components/NewsSletter.jsx
+++ frontend/src/components/NewsSletter.jsx
@@ -9,7 +9,7 @@ export default function Newsletter() {
     ];
 
     return (
-        <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
+        <div className="relative isolate overflow-hidden bg-gray-900 py-10 sm:py-20 lg:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
 
                 {/* Main Container with Two Columns */}
